fe0dab77eb114ea6eb866474b64db3e4271c2657
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-18 23:06:31 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,21 +9,23 @@ public class Main {
 
         System.out.println("Cadastro do primeiro jogador:");
         Jogador jogador1 = new Jogador();
-        jogador1.AtribuirValores();
+        jogador1.AtribuirValores(scanner);
         System.out.println();
 
+        System.out.println("Cadastro do segundo jogador:");
         Jogador jogador2 = new Jogador();
-        jogador2.AtribuirValores();
-        jogador2.isGood(jogador2);
-
+        jogador2.AtribuirValores(scanner);
         System.out.println();
 
+        jogador1.isGood();
+        jogador2.isGood();
+
+        System.out.println("\nInformações dos jogadores:");
         System.out.println(jogador1.ExibirValores());
-
         System.out.println();
-
         System.out.println(jogador2.ExibirValores());
 
+        // Somando estatísticas dos jogadores
         Jogador estatisticasTotais = jogador1.somarEstatisticas(jogador2);
         System.out.println("\nEstatísticas somadas dos jogadores:");
         System.out.println(estatisticasTotais.ExibirValores());
