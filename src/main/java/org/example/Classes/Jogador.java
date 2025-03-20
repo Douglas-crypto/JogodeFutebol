08647505c654a66ad70f7c4d384af400c7c8c462
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-20 17:13:12 -0300

    Update Jogador.java

--- src/main/java/org/example/Classes/Jogador.java
+++ src/main/java/org/example/Classes/Jogador.java
@@ -31,23 +31,20 @@ public class Jogador {
     }
 
 
-    public Jogador somaAtributos(Jogador outro) {
-        Jogador resultado = new Jogador();
-        resultado.nome = this.nome + " e " + outro.nome;
-        resultado.jogos = this.jogos + outro.jogos;
-        resultado.gols = this.gols + outro.gols;
-        resultado.assistencias = this.assistencias + outro.assistencias;
-        return resultado;
+   public int[] somaAtributos(Jogador outro) {
+        int somaJogos = this.jogos + outro.jogos;
+        int somaGols = this.gols + outro.gols;
+        int somaAssistencias = this.assistencias + outro.assistencias;
+        return new int[]{somaJogos, somaGols, somaAssistencias};
     }
 
 
-    public void exibirSomaAtributos(Jogador outro) {
-        Jogador soma = this.somaAtributos(outro);
-        System.out.println("Jogadores: " + soma.nome);
-        System.out.println("Soma dos jogos: " + soma.jogos);
-        System.out.println("Soma dos gols: " + soma.gols);
-        System.out.println("Soma das assistências: " + soma.assistencias);
+    public void exibirSomaAtributos(int[] somas) {
+        System.out.println("Soma dos jogos: " + somas[0]);
+        System.out.println("Soma dos gols: " + somas[1]);
+        System.out.println("Soma das assistências: " + somas[2]);
     }
+
 
     public void eBom() {
         if (jogos > 5 && gols > 5 && assistencias > 5) {
