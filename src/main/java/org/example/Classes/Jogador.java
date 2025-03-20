1047a0abc0f28130580a7eda62b5b4b57d838b0c
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-20 16:56:08 -0300

    Update Jogador.java

--- src/main/java/org/example/Classes/Jogador.java
+++ src/main/java/org/example/Classes/Jogador.java
@@ -1,37 +1,37 @@
-package org.example.Classes;
-
+package org.example;
 import java.util.Scanner;
 
 public class Jogador {
-    private static final Scanner scanner = new Scanner(System.in); 
-    private String nome;
-    private int jogos;
-    private int gols;
-    private int assistencias;
 
-    public void atribuirValores() { 
-        System.out.print("Informe o nome do jogador: ");
-        this.nome = scanner.nextLine();
+    String nome;
+    int jogos;
+    int gols;
+    int assistencias;
 
-        System.out.print("Informe a quantidade de jogos jogados: ");
+    public void atribuiDados() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o nome do jogador: ");
+        this.nome = scanner.next();
+
+        System.out.println("Informe a quantidade de jogos: ");
         this.jogos = scanner.nextInt();
 
-        System.out.print("Informe a quantidade de gols feitos: ");
+        System.out.println("Informe a quantidade de gols feita: ");
         this.gols = scanner.nextInt();
 
-        System.out.print("Informe a quantidade de assistências: ");
+        System.out.println("Informe a quantidade de assistências: ");
         this.assistencias = scanner.nextInt();
-        scanner.nextLine(); // Consumir quebra de linha
     }
 
-    public String exibirValores() {
-        return "Nome: " + nome + "\n" +
-               "Jogos: " + jogos + "\n" +
-               "Gols: " + gols + "\n" +
-               "Assistências: " + assistencias;
+    public void exibirDados() {
+        System.out.println("Nome do jogador: " + nome);
+        System.out.println("Quantidade de jogos: " + jogos);
+        System.out.println("Quantidade de gols: " + gols);
+        System.out.println("Quantidade de assistências: " + assistencias);
     }
 
-    public Jogador somarEstatisticas(Jogador outro) {
+
+    public Jogador somaAtributos(Jogador outro) {
         Jogador resultado = new Jogador();
         resultado.nome = this.nome + " e " + outro.nome;
         resultado.jogos = this.jogos + outro.jogos;
@@ -40,9 +40,20 @@ public class Jogador {
         return resultado;
     }
 
-    public boolean isGood() {
-        boolean bomJogador = jogos > 5 && gols > 5 && assistencias > 5;
-        System.out.println(nome + (bomJogador ? " é um bom jogador" : " é um jogador mediano"));
-        return bomJogador;
+
+    public void exibirSomaAtributos(Jogador outro) {
+        Jogador soma = this.somaAtributos(outro);
+        System.out.println("Jogadores: " + soma.nome);
+        System.out.println("Soma dos jogos: " + soma.jogos);
+        System.out.println("Soma dos gols: " + soma.gols);
+        System.out.println("Soma das assistências: " + soma.assistencias);
+    }
+
+    public void eBom() {
+        if (jogos > 5 && gols > 5 && assistencias > 5) {
+            System.out.println("É um bom jogador");
+        } else {
+            System.out.println("É um mal jogador");
+        }
     }
 }
